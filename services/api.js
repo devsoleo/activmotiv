6d5226c221a9b218d7597eef6c29cf08ecb6ff9c
DevSoleo <devsoleo@protonmail.com>
2026-06-17 11:30:48 +0200

    fix: minor fixes, new version tag

--- services/api.js
+++ services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { getStorageItem } from './storage'
 import { callSignOut } from "@/contexts/authManager"
+import { version } from '@/package.json'
 
 const PUBLIC_API_URL = process.env.EXPO_PUBLIC_API_URL
-const APP_VERSION = process.env.npm_package_version
+const APP_VERSION = version
 
 export const api = axios.create({
   baseURL: PUBLIC_API_URL,
